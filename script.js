1932c6b60fc17118f8df4960277b79c3635dd2e6
praveen kumar <praveenshakya1@gmail.com>
2023-01-08 15:26:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ const errorMessage = document.querySelector("#error-message");
 const numOfNotes = document.querySelectorAll(".numofNotes");
 const typeofNotes = ["2000", "500", "100", "20", "10", "5", "1"];
 
+
 /* add event listner */
 checkButton.addEventListener("click", function(){
     /*---Validate Bill Amount and given cash*/
     errorMessage.style.display = "none";
     if(billAmount.value > 0){
         if(cashGiven.value > billAmount.value){
+            console.log(cashGiven.value);
+            console.log(billAmount.value);
             /*----Calculate the amount to be returned-----*/
             let amountToBeReturned = cashGiven.value - billAmount.value;
-            console.log(amountToBeReturned);
-            calculateTheChangeNotes(amountToBeReturned);
+            if(amountToBeReturned > 0)
+                calculateTheChangeNotes(amountToBeReturned);
         }
-        else{
+        else if(cashGiven.value < billAmount.value){
+            console.log(cashGiven.value);
+            console.log(billAmount.value);
             showError("We are running short of staff for dishwashers..!!, Do you want join?")
+            for(let i = 0; i < typeofNotes.length; i++){
+                numOfNotes[i].innerText = 0;
+            }
         }
     }
     else{
